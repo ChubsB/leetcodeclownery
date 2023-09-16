a2829581476f9714b9fef196ab83636a60fb8612
Hussain <hussainimam01@gmail.com>
2023-09-16 18:34:48 +0500

    bubble sort

---
+++ algorithms/bubble_sort.js
@@ -0,0 +1,17 @@
+const array = [5, 25, 1, 22, 21, 1, 92, 51, 7];
+
+const bubble_sort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+		if(array[j] > array[j+1]) {
+			const temp = array[j+1]
+			array[j+1] = array[j]
+			array[j] = temp
+		}
+		console.log("Pass ", i, array)
+	}
+  }
+  return array
+};
+
+console.log(bubble_sort(array))
