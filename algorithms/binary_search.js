94fb47075e4541c09e840b1e37c184f0ef16e43a
Hussain <hussainimam01@gmail.com>
2023-09-16 17:16:23 +0500

    binary search

---
+++ algorithms/binary_search.js
@@ -0,0 +1,27 @@
+const array = [1, 6, 25, 32, 51, 94, 123]
+
+const binary_search = (array, target) => {
+    let lower_bound = 0
+    let upper_bound = array.length - 1
+
+    while(lower_bound <= upper_bound) { 
+        let midpoint = Math.floor(lower_bound + (upper_bound - lower_bound) / 2); 
+        console.log('Midpoint', midpoint);
+
+        if(array[midpoint] == target) {
+            console.log('Final', midpoint);
+            return midpoint;
+        }
+
+        if(array[midpoint] > target) {
+            upper_bound = midpoint - 1; 
+            console.log('Upper', midpoint);
+        } else {
+            lower_bound = midpoint + 1;
+            console.log('Lower', midpoint);
+        }
+    }
+    return -1;
+};
+
+console.log(binary_search(array, 123));
