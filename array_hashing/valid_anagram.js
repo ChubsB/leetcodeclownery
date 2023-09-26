4599ada3b342081ffcbf3a104329c0aab99e1b0d
hussain <hussainimam01@gmail.com>
2023-09-26 14:26:40 +0500

    valid anagram v2

--- array_hashing/valid_anagram.js
+++ array_hashing/valid_anagram.js
@@ -1,31 +1,16 @@
 var isAnagram = function (s, t) {
-  let tallyS = new Map();
-  let tallyT = new Map();
-  for (let i = 0; i < s.length; i++) {
-    if (tallyS.has(s.charAt(i))) {
-      tallyS.set(s.charAt(i), tallyS.get(s.charAt(i)) + 1);
-    } else {
-      tallyS.set(s.charAt(i), 0);
+	if (s.length !== t.length) return false; 
+    let tallyS = new Map();
+    for(let i = 0; i < s.length; i++) {
+        let char = s.charAt(i);
+        tallyS.set(char, (tallyS.get(char) || 0) + 1);
     }
-  }
-  for (let j = 0; j < t.length; j++) {
-    if (tallyT.has(t.charAt(j))) {
-      tallyT.set(t.charAt(j), tallyT.get(t.charAt(j)) + 1);
-    } else {
-      tallyT.set(t.charAt(j), 0);
+    for(let i = 0; i < t.length; i++) {
+        let char = t.charAt(i);
+        if (!tallyS.has(char) || tallyS.get(char) === 0) return false;
+        tallyS.set(char, tallyS.get(char) - 1);
     }
-  }
-  for (const [key, value] of tallyT) {
-	console.log(key, value)
-    if (tallyS.has(key)) {
-      if (value !== tallyS.get(key)) {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-  return true;
+    return true;
 };
 
 // Input: s = "anagram", t = "nagaram"
@@ -34,5 +19,14 @@ var isAnagram = function (s, t) {
 // Input: s = "rat", t = "car"
 // Output: false
 
-// console.log(isAnagram("anagram", "nagaram"))
-console.log("Final", isAnagram("a", "ab"));
+console.log(isAnagram("rat", "car"));
+console.log(isAnagram("anagram", "nagaram"))
+console.log(isAnagram("a", "ab"));
+
+// Brute force 
+// O(n^2) time 
+// I(1) memory 
+
+// Best
+// O(n) time
+// O(n) memory
