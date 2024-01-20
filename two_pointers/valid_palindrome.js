81f21a9087c9335d8bdca70416821d48a988821e
hussain <hussainimam01@gmail.com>
2024-01-20 20:44:42 +0500

    valid palindrome

--- two_pointers/valid_palindrome.js
+++ two_pointers/valid_palindrome.js
@@ -1,12 +1,14 @@
 var isPalindrome = function(s) {
-    const replaced = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    let isValidPalindrome = true
-    for(let i = 0; i <= ~~(replaced.length / 2); i++){
-        if(replaced.charAt(i) !== replaced.charAt((replaced.length - 1) - i)){
-            isValidPalindrome = false
+    const base = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reverseIndex = base.length - 1
+    for(let i = 0; i < base.length; i++) {
+        if(base.charAt(i) !== base.charAt(reverseIndex - i)) {
+            return false
         }
     }
-    return isValidPalindrome
+    return true
 };
 
-console.log(isPalindrome("A man, a plan, a canal: Panama"))
+console.log(isPalindrome("A man, a plan, a canal: Panama")) // true
+console.log(isPalindrome("race a car")) // false
+console.log(isPalindrome(" ")) // true
