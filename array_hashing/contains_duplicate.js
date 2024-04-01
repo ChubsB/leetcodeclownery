97f228f561ad21bc34fade92b74e25c53d45a520
hussain <hussainimam01@gmail.com>
2024-04-01 20:56:33 +0500

    contains duplicate solved

--- array_hashing/contains_duplicate.js
+++ array_hashing/contains_duplicate.js
@@ -1,23 +1,29 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
 var containsDuplicate = function(nums) {
-    let numberList = new Map();
+    let numMap = new Map()
     for(let i = 0; i < nums.length; i++) {
-        if(numberList.has(nums[i])) {
-            numberList.set(nums[i], numberList.get(nums[i]) + 1)
+        if(numMap.has(nums[i])){
             return true
         } else {
-            numberList.set(nums[i], 0)
+            numMap.set(nums[i], 0)
         }
     }
     return false
 };
-    
-// Input: nums = [1,1,1,3,3,4,3,2,4,2]
-// Output: true
-// Input: nums = [1,2,3,4]
-// Output: false
 
-console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+console.log(containsDuplicate([1,2,3,1]))
 console.log(containsDuplicate([1,2,3,4]))
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
 
 // Brute force 
 // O(n^2) time 
