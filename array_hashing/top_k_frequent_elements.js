ea212ea5225856a61d64562805f2293cd6b337d2
hussain <hussainimam01@gmail.com>
2024-04-03 01:50:31 +0500

    top k frequent elements

--- array_hashing/top_k_frequent_elements.js
+++ array_hashing/top_k_frequent_elements.js
@@ -1,14 +1,27 @@
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+// Example 1:
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
 var topKFrequent = function(nums, k) {
-	let num_map = new Map()
-    nums.forEach(num => {
-		if (num_map.has(num)) {
-			num_map.set(num, num_map.get(num) + 1);
+	let numMap = new Map()
+	let ans = []
+	for(let i = 0; i < nums.length; i++) {
+		if(numMap.has(nums[i])) {
+			numMap.set(nums[i], numMap.get(nums[i]) + 1)
 		} else {
-			num_map.set(num, 1);
+			numMap.set(nums[i], 1)
 		}
-	});
-	let sortedOccurences= Array.from(num_map.entries()).sort((a, b) => b[1] - a[1]);
-	return sortedOccurences.slice(0, k).map(item => item[0]);
+	}
+	 let sortedArray = Array.from(numMap).sort((a, b) => b[1] - a[1]);
+	 for(let i = 0; i < k; i++) {
+		 ans.push(sortedArray[i][0]);
+	 }
+	return ans
 };
 
 // Test cases
@@ -19,3 +32,7 @@ console.log(topKFrequent(nums1,k1)); // [1,2]
 let nums2 = [1]
 let k2 = 1
 console.log(topKFrequent(nums2,k2)); // [1]
+
+let nums3 = [1, 2]
+let k3 = 2
+console.log(topKFrequent(nums3,k3)); // []
