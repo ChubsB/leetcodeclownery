741c1fa5c86d8d125dc1915cc52b008b408702e2
hussain <hussainimam01@gmail.com>
2024-06-22 04:04:52 +0500

    binary search

--- binary_search/Easy/binary_search.js
+++ binary_search/Easy/binary_search.js
@@ -15,18 +15,15 @@ var search = function(nums, target) {
     let right = nums.length - 1;
 
     while (left <= right) {
-        let mid = left + Math.floor((right - left) / 2);
-
+        const mid = Math.floor((left + right) / 2);
         if (nums[mid] === target) {
-            return mid; 
-        }
-
-        if (nums[mid] < target) {
-            left = mid + 1;
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1; 
         } else {
-            right = mid - 1; 
+            right = mid - 1;
         }
     }
 
-    return -1; 
-};
+    return -1;
+}
