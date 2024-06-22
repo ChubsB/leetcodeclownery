af3a619cc0a73285b4e5e0fcda2bba6b241d77af
hussain <hussainimam01@gmail.com>
2024-06-22 15:14:52 +0500

    search 2d matrix

--- binary_search/Medium/search_a_2D_matrix.js
+++ binary_search/Medium/search_a_2D_matrix.js
@@ -12,38 +12,41 @@
 // Output: false
 
 var searchMatrix = function(matrix, target) {
-    let min = 0;
-    let max = matrix.length - 1;
-    let selectedRow = -1;
-    let isFound = false;
-
-    while (min <= max) {
-        let mid = Math.floor((min + max) / 2);
-        if (matrix[mid][matrix[mid].length - 1] < target) {
+    let min = 0 
+    let max = matrix.length - 1
+    let chosenRow = 0
+    while(min <= max) {
+        const mid = Math.floor((min + max) / 2);
+         if (matrix[mid][0] === target) {
+            return true;
+        }
+        if (matrix[mid][0] <= target && matrix[mid][matrix[0].length - 1] >= target) {
+            chosenRow = mid
+            break
+        } else if (matrix[mid][0] < target) {
             min = mid + 1;
-        } else if (matrix[mid][0] > target) {
-            max = mid - 1;
         } else {
-            selectedRow = mid;
-            break;
+            max = mid - 1; 
         }
     }
-
-    if (selectedRow == -1) {
-        return false;
-    }
-
-    min = 0;
-    max = matrix[selectedRow].length - 1;
-    while (min <= max) {
-        let mid = Math.floor((min + max) / 2);
-        if (matrix[selectedRow][mid] == target) {
-            return true; 
-        } else if (matrix[selectedRow][mid] > target) {
-            max = mid - 1;
-        } else {
+    min = 0 
+    max = matrix[chosenRow].length - 1
+    while(min <= max) {
+        const mid = Math.floor((min + max) / 2);
+        if (matrix[chosenRow][mid] === target) {
+            return true
+        } else if (matrix[chosenRow][mid] < target) {
             min = mid + 1;
+        } else {
+            max = mid - 1; 
         }
     }
-    return false;
+    
+    return false
 };
+
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
+
+console.log(searchMatrix([[-8,-7,-5,-3,-3,-1,1],[2,2,2,3,3,5,7],[8,9,11,11,13,15,17],[18,18,18,20,20,20,21],[23,24,26,26,26,27,27],[28,29,29,30,32,32,34]], -5))
